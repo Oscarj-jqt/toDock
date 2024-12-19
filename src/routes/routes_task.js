806b16519c23ed82243df7291180378233679001
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-19 16:18:40 +0100

    Route supression fonctionnelle

--- src/routes/routes_task.js
+++ src/routes/routes_task.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Récupérer toutes les tâches
 router.get('/tasks', async (req, res) => {
     try {
-        const tasks = await Task.find({});
+        const tasks = await Task.find();
         res.status(200).send(tasks);
     } catch (error) {
         res.status(500).send(error);
@@ -23,6 +23,25 @@ router.post('/tasks', async (req, res) => {
         res.status(400).send(error);
     }
 });
+
+// Supression de tâche
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedTask = await Task.findByIdAndDelete(id);
+
+        if (!deletedTask) {
+            return res.status(404).json({ error: 'Tâche introuvable' });
+        }
+
+        res.json({ message: 'La tâche sélectionnée est suprimée', deletedTask }); 
+    } catch (err) {
+        res.status(500).json({ error: 'Erreur  de serveur' });
+    }
+});
+
+
+
 
 
 
