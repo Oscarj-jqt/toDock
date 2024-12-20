a53e72567ab31aad3b75b77c96d8201b720c7953
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-20 12:20:54 +0100

    màj serveur

--- server.js
+++ server.js
@@ -4,29 +4,30 @@ const dotenv = require('dotenv');
 const taskRoutes = require('./src/routes/routes_task');
 const path = require('path');
 
+// Chargement des variables d'env
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// Chargement des variables d'env
-dotenv.config();
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-app.use('/tasks', taskRoutes);
+
+
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI || 'mongodb://mongodb:27017/toDock')
 .then(() => console.log('Connecté à MongoDB '))
 .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
 
-
-
+// Intégration des routes dans le serveur
+app.use('/tasks', taskRoutes);
 
 // Lancement du serveur
 app.listen(PORT, () => {
     console.log(`Serveur lancé sur le port ${PORT}`);
 });
 
-// Intégration des routes dans le serveur
-app.use(taskRoutes);
+
+
 
