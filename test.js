b009307ded05ebbce24cf0a27819c04bb6ce6813
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-20 15:08:12 +0100

    config test

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const request = require('supertest');
 const app = require('./server');
 
+
+// On teste la route tasks 
 describe('Test de l\'API /tasks', () => {
+// test de récupération des données
   test('GET /tasks retourne la liste de tâches à faire', async () => {
     const response = await request(app).get('/tasks');
     expect(response.statusCode).toBe(200);
